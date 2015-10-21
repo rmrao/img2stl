e42572d5343a78f896064dc98d19dcb1229124f2
Roshan Rao <roshan_rao@brown.edu>
2015-10-21 19:31:42 -0400

    deleted unnecessary arguments

--- makestl.py
+++ makestl.py
@@ -8,5 +8,5 @@ from meshcreator import to_mesh
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
 		print >> sys.stderr, "Usage: ./img2stl <path_to_image> <new_stl_filename>"
-	img = prepareImg(sys.argv[1], 50, 2)
+	img = prepareImg(sys.argv[1])
 	to_mesh(img, sys.argv[2], 2)
