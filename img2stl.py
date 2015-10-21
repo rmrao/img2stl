3021f53d35e897f8ba1c40cee4179c5e27778362
Roshan Rao <roshan_rao@brown.edu>
2015-10-21 19:15:01 -0400

    script for conversion with default arguments

---
+++ img2stl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+from imageprep import prepareImg
+from meshcreator import to_mesh
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		print >> sys.stderr, "Usage: ./img2stl <path_to_image> <new_stl_filename>"
+	img = prepareImg(sys.argv[1], 50, 2)
+	to_mesh(img, sys.argv[2], 2)
