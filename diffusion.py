8050c2e882e77332ae2691fe6bf65ca962f74e1d
Roshan Rao <roshan_rao@brown.edu>
2015-11-24 19:13:48 -0500

    second randwalk

--- diffusion.py
+++ diffusion.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
+from scipy.ndimage.filters import generic_filter
 
 from imageprep import prepareImg
 
@@ -32,48 +33,82 @@ def diffuse(image, w=[1,1,1,1], source=None):
 	newim[0, xmax] /= float(w[1] + w[2])
 	newim[ymax, xmax] /= float(w[0] + w[2])
 
-	if source != None:
+	if source is not None:
 		newim[source] = image[source]
 
 	return newim
 
 def randwalk(image):
-	return image + (np.random.randint(2, size=image.shape)*2 - 1)
+	return np.random.randint(2, size=image.shape)*2 - 1
 
-def updateFig1(i, anim):
-	global globimage
-	globimage = randwalk(globimage)
-	anim.set_data(globimage)
-	return anim,
+def importance_filter(image):
+	footprint = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+	return np.abs(generic_filter(image, lambda vals: vals.mean(), footprint=footprint) - image)
 
-def updateFig2(i, anim):
+def multrandwalk(image, importance=None):
+	if importance is None:
+		importance = importance_filter(image)
+	randwalk_mask = np.zeros(image.shape, dtype=bool)
+	y, x = np.random.randint(image.shape[0]), np.random.randint(image.shape[1])
+	randwalk_mask[y, x] = True
+	isvalid = lambda (y, x), (ymax, xmax): (0 <= x < xmax) and (0 <= y < ymax)
+	for i in range(10000):
+		neighbors = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
+		nvals = [importance[n] if isvalid(n, image.shape) else 0 for n in neighbors]
+		if not any(nvals):
+			nvals = [1 if isvalid(n, image.shape) else 0 for n in neighbors]
+		probs = np.cumsum(nvals, dtype=np.float64)
+		probs /= probs[-1]
+		index = np.nonzero(probs > np.random.random())[0][0]
+		y, x = neighbors[index]
+		randwalk_mask[y, x] = True
+	return randwalk_mask
+
+
+def updateFig(i, anim, func):
 	global globimage, globsource
-	globimage = diffuse(globimage, [1, 1, 1, 1], globsource)
+	globimage = func(globimage, globsource)
 	anim.set_data(globimage)
 	return anim,
 
-def test1(image, n=100):
-	global globimage
-	globimage = image
-	fig = plt.figure()
-	im = plt.imshow(image, cmap=cm.Greys_r, animated=True)
-	ani = animation.FuncAnimation(fig, updateFig1, frames=xrange(n), fargs=(im,), interval=1)
-	plt.show()
-
-def test2(image, n=100, source=None):
+def test(image, func, n=100, source=None):
 	global globimage, globsource
 	globimage = image
 	globsource = source
 	fig = plt.figure()
 	im = plt.imshow(image, cmap=cm.Greys_r, animated=True)
-	ani = animation.FuncAnimation(fig, updateFig2, frames=xrange(n), fargs=(im,), interval=1)
+	ani = animation.FuncAnimation(fig, updateFig, frames=xrange(n), fargs=(im, func), interval=1)
 	plt.show()
+
+def updateFig2(i, anim, image, importance):
+	global globimage
+	rw = multrandwalk(image, importance)
+	globimage[rw] = image[rw]
+	anim.set_data(globimage)
+	return anim,
+
+
+def test2(image, n=10):
+	global globimage
+	globimage = np.zeros(image.shape) + 50
+	importance = importance_filter(image)
+	fig = plt.figure()
+	im = plt.imshow(image, cmap=cm.Greys_r, animated=True)
+	ani = animation.FuncAnimation(fig, updateFig2, frames=xrange(n), fargs=(im, image, importance), interval=50)
+	plt.show()
+
 
 import sys
 
 if __name__ == '__main__':
 	img = prepareImg('pics/escher.jpg', compress=True)
 	if len(sys.argv) == 2 and sys.argv[1] == 'randwalk':
-		test1(img)
-	else:
+		test(img, lambda image, source: image + randwalk(image))
+	elif len(sys.argv) == 2 and sys.argv[1] == 'diffuse':
+		test(img, lambda image, source: diffuse(image, [1, 1, 1, 1], source))
+	elif len(sys.argv) == 2 and sys.argv[1] == 'both':
+		test(img, lambda image, source: diffuse(image, [1, 1, 1, 1], source) + randwalk(image))
+	elif len(sys.argv) == 2 and sys.argv[1] == 'randwalk2':
 		test2(img)
+	else:
+		print "Unknown test"
